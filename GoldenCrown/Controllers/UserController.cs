a4067f13e088d3f049d0937cc1cf8fef4267022e
AlexandrIsmagilov <prosto.canek22@gmail.com>
2026-05-04 20:28:09 +0700

    Created UserController

---
+++ GoldenCrown/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using GoldenCrown.DTOs;
+using GoldenCrown.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldenCrown.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        {
+            var result = await _userService.RegisterAsync(request.Login,request.Name,request.Password);
+            if(result)
+            {
+                return Ok();
+                
+            }
+            return BadRequest(new { Message = "User registration failde" });
+        }
+
+    }
+}
