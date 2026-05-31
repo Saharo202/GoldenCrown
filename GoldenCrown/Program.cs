769efc5f046feebd1e51ed2b68522680ce8272cd
AlexandrIsmagilov <prosto.canek22@gmail.com>
2026-05-31 22:16:00 +0700

    Fix SwaggerUI

--- GoldenCrown/Program.cs
+++ GoldenCrown/Program.cs
@@ -23,14 +23,18 @@ namespace GoldenCrown
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                });
             }
 
             app.UseHttpsRedirection();
